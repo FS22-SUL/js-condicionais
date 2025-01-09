e8092ba754c45a45410b8b9cac6e376dae2ca1b2
Francisco Gleidson Teixeira Rocha <Francisco.Rocha@digitalcollege.internal>
2025-01-09 10:25:23 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// if(2 % 3 == 0){
-//     console.log('é par');
-// } else {
-//     console.log('é impar');
-// }
-
 function verificarSemaforo(){
     if (semaforo.value == 'verde'){
         console.log("Siga em frente");
@@ -28,19 +22,56 @@ function gerarNome(){
 
     if(dia.value >= 1 && dia.value <= 8){
         nome = "Desenvolvedor(a)"
-        console.log("Nome: ",nome);
     }else if(dia.value >= 9 && dia.value <= 18){
         nome = "Programador(a)"
-        console.log("Nome: ",nome);
     }else if(dia.value >= 19 && dia.value <= 26){
         nome = "Estagiario(a)"
-        console.log("Nome: ",nome);
     }else{
         nome = "Senior(a)"
-        console.log("Nome: ",nome);
     }
 
-    resposta.innerHTML = "frase completa"
+    switch(mes.value){
+        case "1":
+            nome += ' bugado(a);';
+        break;
+        case "2":
+            nome += ' do CTRL C, CTRL V;';
+        break;
+        case "3":
+            nome += ' das gambiarras;';
+        break;
+        case "4":
+            nome += ' que culpa o cache;';
+        break;
+        case "5":
+            nome += ' que esquece o que faz;';
+        break;
+        case "6":
+            nome += ' do git vazio;';
+        break;
+        case "7":
+            nome += ' das try/catch vazia;';
+        break;
+        case "8":
+            nome += ' famosinho do linkedin;';
+        break;
+        case "9":
+            nome += ' caçador de bugs;';
+        break;
+        case "10":
+            nome += ' do windows pirata;';
+        break;
+        case "11":
+            nome += ' do update sem where;';
+        break;
+        case "12":
+            nome += ' do commit bugado;';
+        break;
+        default:
+            nome = "não veio o mês";
+    }
+
+    resposta.innerHTML = `${nome}`
     
 }
 
